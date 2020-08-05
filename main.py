19f9d203b406e3a3c483ca0db28f90b25ac1c76a
Florent Flament <contact@florentflament.com>
2020-08-05 15:43:27 +0200

    Implemented categorize and save-training commands
    MVP is done

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ def read_lines(filename):
 
 
 class Corpus:
-    def __init_model(self, filename):
-        xy = read_lines(filename)
+    def __learn(self):
+        ## TODO That doesn't need to be performed each time we call __learn
         x,y = [],[]
-        for l in xy:
+        for l in self.__training_set:
             toks = l.split(';')
             x.append(";".join(toks[:-1]))
             y.append(toks[-1])
@@ -31,14 +31,14 @@ class Corpus:
         self.__model = MultinomialNB()
         self.__model.fit(X, y)
 
-    def __init_corpus(self, filename):
-        self.__corpus_str = read_lines(filename)
+        # Project self.__corpus_vec on features extracted by the vectorizer
         self.__corpus_vec = self.__vectorizer.transform(self.__corpus_str)
 
     def __init__(self, training_fname, corpus_fname):
         self.__vectorizer = CountVectorizer()
-        self.__init_model(training_fname)
-        self.__init_corpus(corpus_fname)
+        self.__corpus_str = read_lines(corpus_fname)
+        self.__training_set = read_lines(training_fname)
+        self.__learn()
 
     # Commands
     ##########
@@ -49,6 +49,12 @@ class Corpus:
         with open(filename, "w") as fd:
             for x,y in zip(self.__corpus_str, pred):
                 fd.write("{};{}\n".format(x,y))
+
+    def c_save_training(self, filename):
+        """Overview command"""
+        with open(filename, "w") as fd:
+            for ln in self.__training_set:
+                fd.write("{}\n".format(ln))
 
     def c_overview(self):
         pred = self.__model.predict(self.__corpus_vec)
@@ -65,17 +71,24 @@ class Corpus:
         for x,y in zip(self.__corpus_str, pred):
             c = percat.setdefault(y, [])
             c.append(x)
-        for l in percat[category]:
-            print(l)
+        for item in percat[category]:
+            # TODO Use dictionary to fetch item index
+            print("{:<5} {}".format(self.__corpus_str.index(item), item))
 
+    def c_categorize(self, item_id, category):
+        item = self.__corpus_str[item_id]
+        self.__training_set.append(';'.join([item, category]))
+        self.__learn()
 
 
 def help():
-    msg="""h             Display this help message
-o             Display an overview of the expenses / incomes per category
-p out_fname   Write corpus with predicted categories appended to out_fname file
-l category    List entries in category
-q             Quit"""
+    msg="""h                    Display this help message
+o                    Display an overview of the expenses / incomes per category
+p out_fname          Write corpus with predicted categories appended to out_fname file
+t out_fname          Write training set to out_fname file
+l category           List entries in category
+c item_id category   Classify item with id item_id to category
+q                    Quit"""
     print(msg)
 
 training_fname = sys.argv[1]
@@ -108,6 +121,15 @@ while cmd != 'q':
         except KeyError:
             print("*** wrong category {}".format(category))
             help()
+    elif cmd == 'c':
+        item = int(ln[1])
+        cat  = ln[2]
+        print("*** Classifying {} into {}".format(item, cat))
+        corp.c_categorize(item, cat)
+    elif cmd == 't':
+        out_fname = ln[1]
+        print("*** Saving training file to {}".format(out_fname))
+        corp.c_save_training(out_fname)
     elif cmd == 'h':
         print("*** Help")
         help()
