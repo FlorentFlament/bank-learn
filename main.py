bc8da92076e4a36a36d26cbfd75f54f86a7d93b4
Florent Flament <contact@florentflament.com>
2020-08-05 13:03:52 +0200

    Refactor

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import random
 import sys
 
-import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
@@ -13,48 +12,44 @@ def read_lines(filename):
         lines = [s.strip() for s in fd.readlines()]
     return lines
 
-def random_training(filename, outname, count=10):
-    lines = read_lines(filename)
-    random.shuffle(lines)
-    with open(outname, "a") as fd:
-        for l in lines[:count]:
-            print(l.replace(';', '\n'))
-            category = input("> ")
-            fd.write("{};{}\n".format(l.strip(),category))    
 
-def read_training_set(filename):
-    xy = read_lines(filename)
-    x,y = [],[]
-    for l in xy:
-        toks = l.split(';')
-        x.append(";".join(toks[:-1]))
-        y.append(toks[-1])
-    return x, y
+class Corpus:
+    def __init_model(self, filename):
+        xy = read_lines(filename)
+        x,y = [],[]
+        for l in xy:
+            toks = l.split(';')
+            x.append(";".join(toks[:-1]))
+            y.append(toks[-1])
 
+        # Bag of Words representation
+        # https://scikit-learn.org/stable/modules/feature_extraction.html#text-feature-extraction
+        X = self.__vectorizer.fit_transform(x)
+
+        # Using Naive Bayes classifier for multinomial models
+        # https://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.MultinomialNB.html#sklearn.naive_bayes.MultinomialNB
+        self.__model = MultinomialNB()
+        self.__model.fit(X, y)
+
+    def __init_corpus(self, filename):
+        self.__corpus_str = read_lines(filename)
+        self.__corpus_vec = self.__vectorizer.transform(self.__corpus_str)
+
+    def __init__(self, training_fname, corpus_fname):
+        self.__vectorizer = CountVectorizer()
+        self.__init_model(training_fname)
+        self.__init_corpus(corpus_fname)
+
+    # Commands
+    def c_overview(self):
+        """Overview command"""
+        pred = self.__model.predict(self.__corpus_vec)
+        print("date;type0;type1;details;amount;category")
+        for x,y in zip(self.__corpus_str, pred):
+            print("{};{}".format(x,y))
 
 
 training_fname = sys.argv[1]
-prod_fname = sys.argv[2]
-
-#random_training(prod_fname, training_fname)
-#exit()
-
-train_x, train_y = read_training_set(training_fname)
-
-# Bag of Words representation
-# https://scikit-learn.org/stable/modules/feature_extraction.html#text-feature-extraction
-vectorizer = CountVectorizer()
-train_X = vectorizer.fit_transform(train_x)
-
-# Using Naive Bayes classifier for multinomial models
-# https://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.MultinomialNB.html#sklearn.naive_bayes.MultinomialNB
-clf = MultinomialNB()
-clf.fit(train_X, train_y)
-
-prod_x = read_lines(prod_fname)
-prod_X = vectorizer.transform(prod_x)
-pred = clf.predict(prod_X)
-
-print("date;type0;type1;details;amount;category")
-for x,y in zip(prod_x, pred):
-    print("{};{}".format(x,y))
+corpus_fname   = sys.argv[2]
+corp = Corpus(training_fname, corpus_fname)
+corp.c_overview()
