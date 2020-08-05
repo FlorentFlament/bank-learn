e0930322a1a4d6b50a28303370780bf20ef6f79e
Florent Flament <contact@florentflament.com>
2020-08-05 20:11:51 +0200

    Simplify using pipeline

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ import sys
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
 
 def read_lines(filename):
     lines = None
     with open(filename) as fd:
-        fd.readline() # Consume header line
         lines = [s.strip() for s in fd.readlines()]
     return lines
 
 
 class Corpus:
-    def __learn(self):
+    def __predict(self):
         ## TODO That doesn't need to be performed each time we call __learn
         x,y = [],[]
         for l in self.__training_set:
@@ -22,63 +22,54 @@ class Corpus:
             x.append(";".join(toks[:-1]))
             y.append(toks[-1])
 
-        # Bag of Words representation
-        # https://scikit-learn.org/stable/modules/feature_extraction.html#text-feature-extraction
-        X = self.__vectorizer.fit_transform(x)
-
-        # Using Naive Bayes classifier for multinomial models
-        # https://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.MultinomialNB.html#sklearn.naive_bayes.MultinomialNB
-        self.__model = MultinomialNB()
-        self.__model.fit(X, y)
-
-        # Project self.__corpus_vec on features extracted by the vectorizer
-        self.__corpus_vec = self.__vectorizer.transform(self.__corpus_str)
+        # https://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
+        self.__text_clf.fit(x, y)
+        self.__prediction = self.__text_clf.predict(self.__corpus)
+        print(self.__prediction)
 
     def __init__(self, training_fname, corpus_fname):
-        self.__vectorizer = CountVectorizer()
-        self.__corpus_str = read_lines(corpus_fname)
         self.__training_set = read_lines(training_fname)
-        self.__learn()
+        self.__corpus = read_lines(corpus_fname)
+        self.__text_clf = Pipeline([
+            ('vect', CountVectorizer()),
+            ('clf', MultinomialNB()),
+        ])
+        self.__predict()
 
     # Commands
     ##########
 
     def c_save_prediction(self, filename):
-        """Overview command"""
-        pred = self.__model.predict(self.__corpus_vec)
         with open(filename, "w") as fd:
-            for x,y in zip(self.__corpus_str, pred):
+            for x,y in zip(self.__corpus, self.__prediction):
                 fd.write("{};{}\n".format(x,y))
 
     def c_save_training(self, filename):
-        """Overview command"""
         with open(filename, "w") as fd:
             for ln in self.__training_set:
                 fd.write("{}\n".format(ln))
 
     def c_overview(self):
-        pred = self.__model.predict(self.__corpus_vec)
-        vals = [float(s.split(';')[-1].replace(' ','')) for s in self.__corpus_str]
+        vals = [float(s.split(';')[-1].replace(' ','')) for s in self.__corpus]
         percat = {}
-        for x,y in zip(vals, pred):
+        for x,y in zip(vals, self.__prediction):
             percat[y] = percat.get(y, 0) + x
         for c,v in sorted(percat.items(), key=lambda x:x[1]):
             print("{:<10} {}".format(round(v,2), c))
 
     def c_list_category(self, category):
-        pred = self.__model.predict(self.__corpus_vec)
         percat = {}
-        for x,y in zip(self.__corpus_str, pred):
+        for x,y in zip(self.__corpus, self.__prediction):
             c = percat.setdefault(y, [])
             c.append(x)
         for item in percat[category]:
             # TODO Use dictionary to fetch item index
-            print("{:<5} {}".format(self.__corpus_str.index(item), item))
+            print("{:<5} {}".format(self.__corpus.index(item), item))
 
     def c_categorize(self, item_id, category):
-        item = self.__corpus_str[item_id]
+        item = self.__corpus[item_id]
         self.__training_set.append(';'.join([item, category]))
-        self.__learn()
+        self.__predict()
 
 
 def help():
